453a213c43824aeb9ea31a6398f10fa43384ccf8
lanceli <lanceli1001x@hotmail.com>
2025-05-23 11:04:21 +0800

    fix:gradle config

---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "android-seraphim-music"
+//include(":app")
+//include(":shared")
+//include(":utils")
+//include(":core:ui",":core:permission")
+//include(":domain:scaffold:ui")
+
+check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
+    """
+    Now in Android requires JDK 17+ but it is currently using JDK ${JavaVersion.current()}.
+    Java Home: [${System.getProperty("java.home")}]
+    https://developer.android.com/build/jdks#jdk-config-in-studio
+    """.trimIndent()
+}
